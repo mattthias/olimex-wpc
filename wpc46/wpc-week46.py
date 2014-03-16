ae9efdc54b255c3bb9274ab9fb91bc2fc9124abd
Matthias Schmitz <matthias@sigxcpu.org>
2014-03-16 20:37:23 +0100

    Add wpc46

---
+++ wpc46/wpc-week46.py
@@ -0,0 +1,82 @@
+#!/usr/bin/pthon
+# encoding: utf8
+'''
+Weekend Programming Challenge week 46
+http://olimex.wordpress.com/2014/03/14/weekend-programming-challenge-week-46-clock-arrows/
+Matthias Schmitz <matthias@sigxcpu.org>
+
+The hour hand needs 12h to travel 360° (30°/h , 0.5°/min, 0,008333333°/sec ).
+
+The minute hand needs one hour for the full 360° (6°/min, 0.1°/sec).
+
+We can calculate the angle for the both clock hands for every second and
+compare them. When the angles are equal the clock hands meet each other.
+
+'''
+
+# constants how many degree per second the hour hand move
+HOUR_DEG_PER_SEC = 360.0 / 12 / 60 / 60  # 0.008333...°
+
+
+def minute_to_degree(minute, second=0):
+    ''' (int, int) -> float
+    Calculate the angle of the minute clock hand from a given minute.
+    If no second is provided 0 is assumed.
+
+    >>> minute_to_degree(0)
+    0.0
+    >>> minute_to_degree(12)
+    72.0
+    >>> minute_to_degree(35, 45)
+    214.5
+    '''
+    return (minute * 6.0 + second * 0.1)
+
+
+def hour_to_degree(hour, minute=0, second=0):
+    ''' (int, int, int) -> float
+    Calculate the angle of the hour clock hand for a given hour,
+    minute and second.
+    >>> hour_to_degree(0, 0)
+    0.0
+    >>> hour_to_degree(11, 59)
+    359.5
+    '''
+    return ((hour * 30.0) + (minute * 0.5) + (second * HOUR_DEG_PER_SEC))
+
+
+def time_to_degrees(hour, minute, second):
+    ''' (int, int, int) -> tuple
+    Calculate the angles of hour hand, minute hand and second hand for
+    a given time.
+
+    Returns a tuple containing the angles for hour, minute and second hand:
+    >>> time_to_degrees(0, 0, 0)
+    (0.0, 0.0, 0.0)
+    >>> time_to_degrees(6, 6, 6)
+    (183.05, 36.6, 36.0)
+    >>> time_to_degrees(11, 59, 59)
+    (359.9916666666667, 359.9, 354.0)
+
+    '''
+
+    return ((hour_to_degree(hour, minute, second),
+            minute_to_degree(minute, second),
+            second * 6.0))
+
+
+def main():
+    for hour in range(0, 12):
+        for minute in range(0, 60):
+            for second in range(0, 60):
+                angle_hour, angle_minute, c = time_to_degrees(hour,
+                                                              minute,
+                                                              second)
+                str_hour = "{0:.1f}".format(angle_hour)
+                str_minute = "{0:.1f}".format(angle_minute)
+                if str_hour == str_minute:
+                    print "%02i:%02i:%02i" % (hour, minute, second)
+
+
+if __name__ == '__main__':
+    main()
